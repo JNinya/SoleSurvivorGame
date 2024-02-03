8e7a34cff64aaffb1a5b5c5bddfe854aa4def7f7
avaerian <55154263+avaerian@users.noreply.github.com>
2024-02-02 22:05:00 -0500

    Small update to comments

--- fh.py
+++ fh.py
@@ -7,14 +7,14 @@ write(filename, text)
 
 """
 
-#returns the text of a text file
+# Returns the text of a text file
 def read(filename):
     file = open(filename, "r")
     output = file.read()
     file.close()
     return output
 
-#overwrites the text of a text file. creates a new file if one doesn't exist
+# Overwrites the text of a text file. Creates a new file if one doesn't exist
 def write(filename, text):
     file = open(filename, "w")
     output = file.write(text)
